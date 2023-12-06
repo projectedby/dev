7fde1be2ff4a0ae1b90af00828dbe731d2211627
novemberizing <novemberizing@gmail.com>
2023-12-06 12:54:29 +0900

    Update dev.

--- 2023/12/readv.c
+++ 2023/12/readv.c
@@ -1,51 +1,45 @@
 #include <sys/time.h>
-#include <stdio.h>
 #include <sys/uio.h>
 #include <sys/stat.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
 
 int main() {
     struct timeval start;
     struct timeval end;
 
-    long diff = 0;
     long final = 0;
+    long diff = 0;
 
+    struct iovec iov[1024];
     char buf[1024][25] = { 0, };
-    ssize_t n;
 
     for(int i = 0; i < 10; i++) {
         gettimeofday(&start, NULL);
-
         int fd = open("hello.txt", O_RDONLY);
-        if(fd == -1) {
-            printf("error\n");
-            return fd;
-        }
+
+        if(fd == -1) return -1;
 
         for(int j = 0; j < 1024; j++) {
-            ssize_t n = read(fd, buf[j], 24);
-            if(n == -1) {
-                printf("error\n");
-                return n;
-            }
+            iov[j].iov_base = buf[j];
+            iov[j].iov_len = 24;
         }
 
-        if(close(fd)) {
-            printf("close error\n");
+        ssize_t n = readv(fd, iov, 1024);
+        if(n < 0) {
             return -1;
         }
 
-        gettimeofday(&end, NULL);
+        close(fd);
 
+        gettimeofday(&end, NULL);
         diff = (end.tv_sec - start.tv_sec) * 1000000 + end.tv_usec - start.tv_usec;
         final = final + diff;
-
-        printf("[%d] elapsed time: %lus\n", i + 1, diff);
+        printf("[%d] elapsed time: %lds\n", i, diff);
     }
 
-    printf("[avg] elapsed time: %fs\n", (double)(final / 10.0f));
+    printf("[avg] elapsed time: %f\n", (double) (final / 10));
 
     return 0;
 }
